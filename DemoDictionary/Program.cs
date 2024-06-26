4a0794c7275971577dc82b7990126b45d563d020
Muhammed Mustafa Bardakci <bardakcimmustafa@gmail.com>
2024-06-26 14:06:24 +0300

    Add project files.

---
+++ DemoDictionary/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<int, string> MyDictionary = new();
+        MyDictionary.Add(01, "Adana");
+        MyDictionary.Add(06, "Ankara");
+        MyDictionary.Add(34, "İstanbul");
+        MyDictionary.Add(42, "Konya");
+
+        foreach (var (key, value) in MyDictionary)
+        {
+            Console.WriteLine($"{key} şehri : {value}");
+        }
+    }
+}
